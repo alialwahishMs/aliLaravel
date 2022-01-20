adac10d02923baf8fab199b476ad7a1c9f1a7ab8
alialwahishMs <96257153+alialwahishMs@users.noreply.github.com>
2022-01-20 16:06:46 -0600

    Update session.php

--- config/session.php
+++ config/session.php
@@ -168,8 +168,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE'),
-
+    'secure' => env('SESSION_SECURE_COOKIE', null),
     /*
     |--------------------------------------------------------------------------
     | HTTP Access Only
