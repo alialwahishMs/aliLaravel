3febf3ba150c149ac48c757f9ea851fdbd385a17
alialwahishMs <v-alibayati@microsoft.com>
2022-01-25 13:33:48 -0600

    finalizing the startup file

--- webAppStartup.sh
+++ webAppStartup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash 
 
-sed 's/wwwroot/wwwroot\/public/' /etc/nginx/sites-enable/default
+# make sure you've copied the default file from the nginx site-enabled and edited the root directory to 
+#/home/site/wwwroot/public 
+cp /home/site/default /etc/nginx/sites-available/default
 service nginx reload
 
-cd /home/site/wwwroot && cp -n .env.examples .env 
+cd /home/site/wwwroot && cp -n .env.example .env 
 php artisan key:generate
